cf6c76d6c2ba9194c0b7c6e85aeb27d45a7d28dc
nikipi <61734290+nikipi@users.noreply.github.com>
2020-04-07 16:45:37 +0200

    Add files via upload

--- k-fold_cross validation.py
+++ k-fold_cross validation.py
@@ -109,3 +109,53 @@ plt.show()
 score = model.score(X_test, Y_test)
 
 print(score)
+
+
+###############
+#K FOLD########
+###############
+
+# Import the necessary modules
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Compute 5-fold cross-validation scores: cv_scores
+cv_scores = cross_val_score(reg,X,y,cv=5)
+
+# Print the 5-fold cross-validation scores
+print(cv_scores)
+
+print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
+
+
+
+##########################
+#train/test split for reg#
+##########################
+
+# Import necessary modules
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# Create training and test sets
+X_train, X_test, y_train, y_test =train_test_split(X, y, test_size = 0.3, random_state=42)
+
+# Create the regressor: reg_all
+reg_all = LinearRegression()
+
+# Fit the regressor to the training data
+reg_all.fit(X_train,y_train)
+
+# Predict on the test data: y_pred
+y_pred = reg_all.predict(X_test)
+
+# Compute and print R^2 and RMSE
+print("R^2: {}".format(reg_all.score(X_test, y_test)))
+rmse = np.sqrt(mean_squared_error(y_pred, y_test))
+print("Root Mean Squared Error: {}".format(rmse))
